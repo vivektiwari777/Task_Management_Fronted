3c9e16de3bb00628925aeae0856cc9d3db1175d1
vivek tiwari <125742021+vivektiwari777@users.noreply.github.com>
2025-02-21 17:54:43 +0530

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -15,7 +15,7 @@ const Login = () => {
         setError("");
 
         try {
-            const response = await fetch("http://localhost:5000/api/auth/login", {
+            const response = await fetch("https://task-management-system-backend-1.onrender.com/api/auth/login", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ email, password }),
